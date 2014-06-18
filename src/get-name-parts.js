c51ba30fa2d03c5d927bc3e43eeff07754a5d2e5
Benjamin Horsleben <benjamin@fizkerinc.dk>
2014-06-18 23:33:36 +0200

    Removed files that were no longer used.

--- src/get-name-parts.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(dep) {
-	var split = dep.split('!')
-	if(split.length == 1) {
-		return { plugin: '', name: dep }
-	} else {
-		return { plugin: split[0], name: split[1] }
-	}
-}
