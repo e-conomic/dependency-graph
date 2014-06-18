705d34513df9c844233c614e477c3cc568aaf95b
Benjamin Horsleben <benjamin@fizkerinc.dk>
2014-06-18 23:33:13 +0200

    Fixed some issues with excluded dependencies.

--- index.js
+++ index.js
@@ -37,18 +37,42 @@ if(anonymous.length > 0) {
 		}))
 }
 
+var excludedModules = []
+
 modules = modules
 	// Find entry points
 	.map(function(module) {
+		// Find the relative url
+		var base = path.dirname(module.name)
+
+		// Resolve relative dependencies
 		var ret = {
 			name: module.name,
-			dependencies: module.deps,
+			dependencies: module.deps.map(function(dep) {
+				if(dep[0] == '.') {
+					dep = path.join(base, dep)
+				}
+
+				// Discover if the module exists, or has been excluded
+				if(!~excludedModules.indexOf(dep) && !modules.find(function(module) {
+					return module.name == dep
+				})) {
+					excludedModules.push(dep)
+				}
+
+				return dep
+			}),
 		}
+
+		// Set entry if this in an entry point
 		if(~entryPoints.indexOf(module.name)) {
 			ret.entry = true
 		}
 		return ret
 	})
+	.concat(excludedModules.map(function(dep) {
+		return { name: dep, dependencies: [] }
+	}))
 
 
 neo(modules)
